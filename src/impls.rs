35f85401837e2365b6822da41bf399cb837dee1a
Милош Чутурић <miloscuturic8@gmail.com>
2025-07-23 15:14:31 +0200

    Implemented nft marketplace

--- src/impls.rs
+++ src/impls.rs
@@ -1,14 +1,11 @@
 use super::*;
 use frame::prelude::*;
 use frame::primitives::BlakeTwo256;
+use frame::traits::tokens::Preservation;
 use frame::traits::Hash;
 
-
 impl<T: Config> Pallet<T> {
-	
 	pub fn gen_dna() -> [u8; 32] {
-		
-		
 		let unique_payload = (
 			frame_system::Pallet::<T>::parent_hash(),
 			frame_system::Pallet::<T>::block_number(),
@@ -20,8 +17,8 @@ impl<T: Config> Pallet<T> {
 	}
 
 	pub fn mint(owner: T::AccountId, dna: [u8; 32]) -> DispatchResult {
-		let kitty = Kitty { dna, owner: owner.clone() };
-		
+		let kitty = Kitty { dna, owner: owner.clone(), price: None };
+
 		ensure!(!Kitties::<T>::contains_key(dna), Error::<T>::DuplicateKitty);
 
 		let current_count: u32 = CountForKitties::<T>::get();
@@ -32,6 +29,61 @@ impl<T: Config> Pallet<T> {
 		CountForKitties::<T>::set(new_count);
 
 		Self::deposit_event(Event::<T>::Created { owner });
+		Ok(())
+	}
+
+	pub fn do_transfer(from: T::AccountId, to: T::AccountId, kitty_id: [u8; 32]) -> DispatchResult {
+		ensure!(from != to, Error::<T>::TransferToSelf);
+		let mut kitty = Kitties::<T>::get(kitty_id).ok_or(Error::<T>::NoKitty)?;
+		ensure!(kitty.owner == from, Error::<T>::NotOwner);
+
+		kitty.owner = to.clone();
+		kitty.price = None;
+		let mut to_owned = KittiesOwned::<T>::get(to.clone());
+		to_owned.try_push(kitty_id).map_err(|_| Error::<T>::TooManyOwned)?;
+
+		let mut from_owned = KittiesOwned::<T>::get(from.clone());
+		if let Some(ind) = from_owned.iter().position(|&id| id == kitty_id) {
+			from_owned.swap_remove(ind);
+		} else {
+			return Err(Error::<T>::NoKitty.into());
+		}
+
+		Kitties::<T>::insert(kitty_id, kitty);
+		KittiesOwned::<T>::insert(&to, to_owned);
+		KittiesOwned::<T>::insert(&from, from_owned);
+
+		Self::deposit_event(Event::<T>::Transferred { from, to, kitty_id });
+		Ok(())
+	}
+
+	pub fn do_set_price(
+		caller: T::AccountId,
+		kitty_id: [u8; 32],
+		new_price: Option<BalanceOf<T>>,
+	) -> DispatchResult {
+		let mut kitty = Kitties::<T>::get(kitty_id).ok_or(Error::<T>::NoKitty)?;
+		ensure!(kitty.owner == caller, Error::<T>::NotOwner);
+		kitty.price = new_price;
+		Kitties::<T>::insert(kitty_id, kitty);
+
+		Self::deposit_event(Event::<T>::PriceSet { owner: caller, kitty_id, new_price });
+		Ok(())
+	}
+
+	pub fn do_buy_kitty(
+		buyer: T::AccountId,
+		kitty_id: [u8; 32],
+		price: BalanceOf<T>,
+	) -> DispatchResult {
+		let kitty = Kitties::<T>::get(kitty_id).ok_or(Error::<T>::NoKitty)?;
+		let real_price = kitty.price.ok_or(Error::<T>::NotForSale)?;
+		ensure!(price >= real_price, Error::<T>::MaxPriceTooLow);
+
+		T::NativeBalance::transfer(&buyer, &kitty.owner, real_price, Preservation::Preserve)?;
+		Self::do_transfer(kitty.owner, buyer.clone(), kitty_id)?;
+
+		Self::deposit_event(Event::<T>::Sold { buyer, kitty_id, price: real_price });
 		Ok(())
 	}
 }
